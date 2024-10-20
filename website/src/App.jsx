c10909764690667c0354517e34a53bdc075977dd
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-10-20 21:16:43 +0900

    Add icons

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,18 +3,24 @@ import './App.css'
 function App() {
   return (
     <>
-      <div class="grid grid-cols-4 gap-4 m-4">
+      <div className="grid grid-cols-4 gap-4 m-4">
         <div className="card bg-base-100 w-auto shadow-xl">
           <figure>
             <img
-              src="https://cdn.jsdelivr.net/gh/rivercory/3d-social-media-icons/website/src/assets/icons/social=youtube.svg"
-              alt="Shoes" />
+              src="https://cdn.jsdelivr.net/gh/rivercory/3d-social-media-icons/website/public/icons/social-youtube.svg"
+              alt="YouTube icon" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">Youtube</h2>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <div className="dropdown dropdown-bottom dropdown-end">
+                <div tabIndex={0} role="button" className="btn m-1">Click</div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-36 p-2 shadow">
+                  <li><a>.png</a></li>
+                  <li><a>.jpg</a></li>
+                  <li><a>.svg</a></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
